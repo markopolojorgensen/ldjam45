734bc8b4a123827a3d16cf0661ca06b0eb44d9d7
markopolodev <marko.polo.jorgensen@gmail.com>
2019-10-04 00:06:54 -0500

    defualt personal addons

---
+++ godot_project_folder/addons/convenient_state_machine/state_machine.gd
@@ -0,0 +1,52 @@
+extends Node
+
+export(bool) var debug = false
+
+var current_state setget set_current_state
+
+var transitions = {}
+
+var state_names = {}
+
+func on_transition(from_state, to_state, object, method):
+	if not transitions.has(from_state):
+		transitions[from_state] = {}
+	
+	transitions[from_state][to_state] = [object, method]
+
+func set_current_state(new_state):
+	# initial state, don't bother with transitioning from null
+	if current_state == null:
+		current_state = new_state
+		return
+	
+	if not transitions.has(current_state):
+		if debug:
+			var message = "no transitions out of state %s (to %s or anything), not transitioning"
+			print(message % [name_for(current_state), name_for(new_state)])
+		return
+	
+	if not transitions[current_state].has(new_state):
+		if debug:
+			var message = "no transition from %s to %s specified"
+			print(message % [name_for(current_state), name_for(new_state)])
+		return
+	
+	var object = transitions[current_state][new_state][0]
+	var method = transitions[current_state][new_state][1]
+	
+	if not object.has_method(method):
+		# always complain about this
+		print("%s doesn't have method %s" % [str(object), method])
+		return
+	else:
+		object.call(method)
+		if debug:
+			print("transitioned from %s to %s" % [name_for(current_state), name_for(new_state)])
+		current_state = new_state
+
+func name_for(state):
+	if state_names.has(state):
+		return state_names[state]
+	else:
+		return str(state)
